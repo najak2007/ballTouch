a6716c992b172df0fb435b9fb060d2055adbe93d
najak <najak2007@gmail.com>
2025-10-01 18:01:32 +0900

    프로젝트 생성

---
+++ ballTouch/ballTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballTouchApp.swift
+//  ballTouch
+//
+//  Created by najak on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ballTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
